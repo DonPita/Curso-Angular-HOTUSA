ba432ebac5aa39109ae828b6717423a13ecc8950
Adrian Pita <pitabermudez@gmail.com>
2024-05-02 15:48:40 +0200

    Leccion 25, clases

--- 01-typescript-intro/src/topics/08-classes.ts
+++ 01-typescript-intro/src/topics/08-classes.ts
@@ -1,5 +1,20 @@
 
 export class Person {
 
-    
+    public name: string;
+    private address: string;
+
+    constructor() {
+        this.name = 'Fernando';
+        this.address = 'Lugo';
+    }
+
+
 }
+
+const ironman = new Person();
+
+/*Da error, porque es privado, no deberia funcionar, pero funciona.
+console.log(ironman.address);*/
+
+console.log(ironman);
