9c96e026ee86b822e7500991b069ed3acb1c5d7d
Adrian Pita <pitabermudez@gmail.com>
2024-05-02 13:19:06 +0200

    Tarea 19, objetos e interfaz para objetos

---
+++ 01-typescript-intro/src/topics/04-homework-types.ts
@@ -0,0 +1,44 @@
+/*
+    ===== Código de TypeScript =====
+*/
+//Interfaz SuperHero
+interface SuperHero {
+    name: string;
+    age: number;
+    address: Address;
+    showAddress: () => string;
+}
+
+//Declaramos la direccion como interfaz, asi se puede reutilizar
+interface Address {
+    street: string;
+    country: string;
+    city: string
+}
+
+
+const superHeroe: SuperHero = {
+    name: 'Spiderman',
+    age: 30,
+    address: {
+        street: 'Main St',
+        country: 'USA',
+        city: 'NY'
+    },
+    showAddress() {
+        return this.name + ', ' + this.address.city + ', ' + this.address.country;
+    }
+}
+
+
+const address = superHeroe.showAddress();
+console.log(address);
+
+
+
+
+export { };
+
+
+
+
