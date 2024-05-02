77dc4ec314d959d3e5b105c2673a164e194c5128
Adrian Pita <pitabermudez@gmail.com>
2024-05-02 13:48:43 +0200

    Leccion 20, destructuración

---
+++ 01-typescript-intro/src/topics/05-basic-destructuring.ts
@@ -0,0 +1,45 @@
+
+interface AudioPlayer {
+    audioVolume: number;
+    songDuration: number;
+    song: string;
+    details: Details;
+}
+
+interface Details {
+    author: string;
+    year: number;
+}
+
+const audioPlayer: AudioPlayer = {
+    audioVolume: 90,
+    songDuration: 36,
+    song: "Costa del Silencio",
+    details: {
+        author: 'Mago de Oz',
+        year: 2007
+    }
+}
+
+//Destructuracion, lo comentado es otra opcion
+const { song: anotherSong,
+    songDuration: duration,
+    details
+    // details: {author}
+} = audioPlayer;
+
+const { author } = details; //La opcion comentada en la linea 28, significa borrar el siguiente
+
+/* Sin destructurar
+console.log('Song: ', audioPlayer.song);
+console.log('Duration: ', audioPlayer.songDuration);
+console.log('Author : ', audioPlayer.details.author);*/
+
+//Desestructurado
+console.log('Song: ', anotherSong);
+console.log('Duration: ', duration);
+console.log('Author : ', author);
+
+
+
+export { };
