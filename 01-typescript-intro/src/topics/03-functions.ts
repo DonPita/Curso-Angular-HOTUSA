1815c75c1e2cac41bee901826ee67628934ea2a7
Adrian Pita <pitabermudez@gmail.com>
2024-05-02 12:59:08 +0200

    Leccion 18

--- 01-typescript-intro/src/topics/03-functions.ts
+++ 01-typescript-intro/src/topics/03-functions.ts
@@ -13,14 +13,39 @@ function multiply(firstNumber: number, secondNumber?: number, base: number = 2) 
     return firstNumber * base;
 }
 
-
-const result: number = addNumbers(1, 2);
+/*const result: number = addNumbers(1, 2);
 const result2: string = addNumbersArrow(1, 2);
 const multiplyResult: number = multiply(5);
 
 
 //Al usar llaves en el log, se imprime como un objeto
-console.log({ result, result2, multiplyResult });
+console.log({ result, result2, multiplyResult });*/
+
+//Interfaz para el personaje
+interface Character {
+    name: string;
+    hp: number;
+    showHp: () => void;
+}
+
+//metodo para curar personaje
+const healCharacter = (character: Character, amount: number) => {
+
+    character.hp += amount;
+}
+
+//Creación personaje
+const strider: Character = {
+    name: 'Strider',
+    hp: 50,
+    showHp() {
+        console.log(`Puntos de vida ${this.hp}`);
+    }
+}
+
+healCharacter(strider, 10);
+
+strider.showHp();
 
 
 
