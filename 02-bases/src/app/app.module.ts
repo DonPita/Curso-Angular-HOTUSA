5665ddac9526a4d25a8df5415d77f79d064db118
Adrian Pita <pitabermudez@gmail.com>
2024-05-03 13:15:35 +0200

    Creacion componentes con comandos, Heroes

--- 02-bases/src/app/app.module.ts
+++ 02-bases/src/app/app.module.ts
@@ -4,11 +4,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CounterComponent } from './counter/counter.component';
+import { HeroComponent } from './heroes/hero/hero.component';
+import { ListComponent } from './heroes/list/list.component';
+
+
 
 @NgModule({
   declarations: [
     AppComponent,
-    CounterComponent //Importacion del counter de la carpeta counter
+    //Importacion del counter de la carpeta counter
+    CounterComponent,
+    HeroComponent,
+    ListComponent,
   ],
   imports: [
     BrowserModule,
