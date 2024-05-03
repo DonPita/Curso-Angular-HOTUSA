50cd339d43a64a02dfb385b5eefe315399744202
Adrian Pita <pitabermudez@gmail.com>
2024-05-03 14:09:19 +0200

    Correccion

--- 02-bases/src/app/heroes/hero/hero.component.ts
+++ 02-bases/src/app/heroes/hero/hero.component.ts
@@ -9,10 +9,12 @@ export class HeroComponent {
   public name: string = 'IronMan';
   public age: number = 45;
 
+  //Esto es como si fuera un atributo
   get capitalizedName(): string {
     return this.name.toUpperCase();
   }
 
+  //Esto ya es un getter en toda regla
   getHeroDescription(): string {
     //return this.name +' - ' + this.age
     return `${this.name} - ${this.age}`
