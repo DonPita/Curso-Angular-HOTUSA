7f676cb90de86676f7a7aa900126c9bc5d19f08b
Adrian Pita <pitabermudez@gmail.com>
2024-05-06 17:38:11 +0200

    Servicios de UUID, hacer la tarea de la lección 72

--- 02-bases/src/app/dbz/services/dbz.service.ts
+++ 02-bases/src/app/dbz/services/dbz.service.ts
@@ -1,23 +1,32 @@
 import { Injectable } from '@angular/core';
+import { v4 as uuid} from 'uuid';
+
 import { Character } from '../interfaces/character.interface';
+
 
 @Injectable({ providedIn: 'root' })
 
 export class DbzService {
 
   public characters: Character[] = [{
+    id: uuid(),
     name: 'Krillin',
     power: 1000
   }, {
+    id: uuid(),
     name: 'Goku',
     power: 9500
   }, {
+    id: uuid(),
     name: 'Vegeta',
     power: 7500
   }];
 
   onNewCharacter(character: Character): void {
-    this.characters.push(character);
+    const newCharacter: Character = {
+      id: uuid(), ...character //Copia todas las caractericas del constructor
+    }
+    this.characters.push(newCharacter);
   }
 
   onDeleteCharacter(index: number): void {
