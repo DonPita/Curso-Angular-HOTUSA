d8e10192bdc527986a8c239cd63c79e4acef5ebf
Adrian Pita <pitabermudez@gmail.com>
2024-05-06 09:21:48 +0200

    Ng-template y ngIf-else

--- 02-bases/src/app/heroes/list/list.component.ts
+++ 02-bases/src/app/heroes/list/list.component.ts
@@ -8,4 +8,13 @@ import { Component } from '@angular/core';
 export class ListComponent {
 
   public heroNames: string[] = ['Spiderman', 'IronMan', 'Hulk', 'Thor', 'AntMan'];
+  public deletedHero?: string;
+
+  removeLastHero(): void {
+    this.deletedHero = this.heroNames.pop();
+    //console.log({ deletedHero });
+  }
+
+
+
 }
