51f5465df33d4b07de8918c79b109a56cab63540
Adrian Pita <pitabermudez@gmail.com>
2024-05-06 11:22:04 +0200

    Módulo DBZ, Tuve que reiniciar el LocalHost, que me daba error el cabrón al importalo, no se porque

--- 02-bases/src/app/app.module.ts
+++ 02-bases/src/app/app.module.ts
@@ -1,14 +1,13 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+import { AppRoutingModule } from './app-routing.module';
 import { CounterModule } from './counter/counter.module';
 import { HeroesModule } from './heroes/heroes.module';
+import { DbzModule } from './dbz/dbz.module';
 
-import { AppRoutingModule } from './app-routing.module';
+
 import { AppComponent } from './app.component';
-
-
-
 
 
 
@@ -16,13 +15,14 @@ import { AppComponent } from './app.component';
 @NgModule({
   declarations: [
     AppComponent,
+
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     CounterModule,
     HeroesModule,
-
+    DbzModule
   ],
   providers: [],
   bootstrap: [AppComponent]
