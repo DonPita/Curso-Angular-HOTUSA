340330e9132cf3b2651686ef13d1affa75b99960
Adrian Pita <pitabermudez@gmail.com>
2024-05-06 09:39:48 +0200

    Encapsulando modulos e importandolos

--- 02-bases/src/app/app.module.ts
+++ 02-bases/src/app/app.module.ts
@@ -1,25 +1,26 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+import { CounterModule } from './counter/counter.module';
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { CounterComponent } from './counter/counter.component';
 import { HeroComponent } from './heroes/hero/hero.component';
 import { ListComponent } from './heroes/list/list.component';
+
 
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    //Importacion del counter de la carpeta counter
-    CounterComponent,
     HeroComponent,
     ListComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    CounterModule
   ],
   providers: [],
   bootstrap: [AppComponent]
