dd3e30c9438b27d5a9aa233d15f5386df643eddc
Adrian Pita <pitabermudez@gmail.com>
2024-05-07 10:27:15 +0200

    Fin seccion 5, clases privadas, getters basicamente

--- 02-bases/src/app/dbz/pages/main-page.component.ts
+++ 02-bases/src/app/dbz/pages/main-page.component.ts
@@ -1,3 +1,5 @@
+import { createInjectableType } from '@angular/compiler';
+import { Character } from '../interfaces/character.interface';
 import { DbzService } from '../services/dbz.service';
 import { Component } from '@angular/core';
 
@@ -10,8 +12,23 @@ import { Component } from '@angular/core';
 export class MainPageComponent {
 
   //Solo con el constructor ya lo puedes usar en el html
-  constructor(public dbzService: DbzService) {
+  constructor(private dbzService: DbzService) {
 
+  }
+
+  get characters(): Character[] {
+    //return this.dbzService.characters; Modo normal
+    /*De la siguiente manera hacemos una copia del Array original por si no
+    queremos que este sea modificado*/
+    return [...this.dbzService.characters];
+  }
+
+  onDeleteCharacter( id: string):void {
+    this.dbzService.deleteCharacterById(id);
+  }
+
+  onNewCharacter(character: Character): void {
+    this.dbzService.addCharacter(character);
   }
 
 }
