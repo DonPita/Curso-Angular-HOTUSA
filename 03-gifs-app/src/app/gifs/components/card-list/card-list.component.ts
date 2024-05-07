bd77590a42459304f6bf6ca69e6d773eb45522d2
Adrian Pita <pitabermudez@gmail.com>
2024-05-07 15:26:49 +0200

    Solucion Tarea anterior y modificacion de HTML para las tarjetas

--- 03-gifs-app/src/app/gifs/components/card-list/card-list.component.ts
+++ 03-gifs-app/src/app/gifs/components/card-list/card-list.component.ts
@@ -3,7 +3,6 @@ import {  Component } from '@angular/core';
 @Component({
   selector: 'gifs-card-list',
   templateUrl: './card-list.component.html',
-  styleUrl: './card-list.component.css',
 
 })
 
