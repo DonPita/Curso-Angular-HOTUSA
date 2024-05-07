4efb79274ec3c11ec2a25b70f35b8f31afc556f3
Adrian Pita <pitabermudez@gmail.com>
2024-05-07 15:43:54 +0200

    @ViewChild para referencia local

--- 03-gifs-app/src/app/gifs/components/search-box/search-box.component.ts
+++ 03-gifs-app/src/app/gifs/components/search-box/search-box.component.ts
@@ -1,16 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'gifs-search-box',
   template: `
   <h5>Buscar:</h5>
-  <input type="text" class="form-control" placeholder="Buscar gifs...">
+  <input type="text" class="form-control" placeholder="Buscar gifs..."
+  (keyup.enter)="searchTag()"
+  #txtTagInput> <!--Referencia para si mismo a la vida buena-->
   `
 })
 
 export class SearchBoxComponent {
 
+  //Permite hacer una referencia en local, al elemento txtTagInput
+  @ViewChild('txtTagInput')
+  // Not Null Operator "!"
+  public tagInput!: ElementRef<HTMLInputElement>;
+
   constructor() { }
+
+  searchTag() {
+    const newTag = this.tagInput.nativeElement.value;
+
+    console.log({ newTag });
+
+  }
 
 
 }
