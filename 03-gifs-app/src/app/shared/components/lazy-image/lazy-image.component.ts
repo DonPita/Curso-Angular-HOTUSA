88a743df023a38644b2f19ce222c27fcec327ebd
Adrian Pita <pitabermudez@gmail.com>
2024-05-08 13:26:23 +0200

    Lazy Image parte 2, importamos un loader

--- 03-gifs-app/src/app/shared/components/lazy-image/lazy-image.component.ts
+++ 03-gifs-app/src/app/shared/components/lazy-image/lazy-image.component.ts
@@ -1,9 +1,31 @@
 
-import { Component } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'shared-lazy-image',
   templateUrl: './lazy-image.component.html',
 })
 
-export class LazyImageComponent { }
+export class LazyImageComponent implements OnInit {
+
+  @Input()
+  public url!: string;
+
+  @Input()
+  public alt: string = '';
+
+  //Determinar que imagen se va a cargar
+  public hasLoaded: boolean = false;
+
+
+
+  ngOnInit(): void {
+    if (!this.url) throw new Error('URL is required');
+  }
+
+
+  onLoad(): void {
+    console.log('Image Loaded');
+    this.hasLoaded = true;
+  }
+}
