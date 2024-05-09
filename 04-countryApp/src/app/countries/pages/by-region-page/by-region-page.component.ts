52944793e9e9e623480aa50a9342fbd312e4a887
Adrian Pita <pitabermudez@gmail.com>
2024-05-09 16:58:32 +0200

    Modificacion del by-region para meter un type con lasl opciones fijas de region con unos botones en el html

--- 04-countryApp/src/app/countries/pages/by-region-page/by-region-page.component.ts
+++ 04-countryApp/src/app/countries/pages/by-region-page/by-region-page.component.ts
@@ -2,19 +2,30 @@ import { Component } from '@angular/core';
 import { Country } from '../../interfaces/country';
 import { CountriesService } from '../../services/countries.service';
 
+
+/*type como un enumerado, pero que no va a ser modificado, tiene que ir antes del
+Component si no da error*/
+type Region = 'Africa' | 'Americas' | 'Asia' | 'Europe' | 'Oceania';
+
 @Component({
   selector: 'app-by-region-page',
   templateUrl: './by-region-page.component.html',
   styles: ``
 })
+
 export class ByRegionPageComponent {
 
   public countries: Country[] = [];
+  public regions: Region[] = ['Africa', 'Americas', 'Asia', 'Europe', 'Oceania'];
+  public selectedRegion?: Region;
 
   constructor(private countriesService: CountriesService) { }
 
-  searchByRegion(term: string) {
-    this.countriesService.searchRegion(term)
+  searchByRegion(region: Region) {
+
+    this.selectedRegion = region;
+
+    this.countriesService.searchRegion(region)
       .subscribe(countries => {
         this.countries = countries;
       });
