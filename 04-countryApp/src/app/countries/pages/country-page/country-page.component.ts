ca7da0a55e870efe5ada3c4efb9057cfe0b7a7ba
Adrian Pita <pitabermudez@gmail.com>
2024-05-09 13:32:33 +0200

    html y componente de country page para mostrar mas info

--- 04-countryApp/src/app/countries/pages/country-page/country-page.component.ts
+++ 04-countryApp/src/app/countries/pages/country-page/country-page.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
 import { CountriesService } from '../../services/countries.service';
 import { switchMap } from 'rxjs';
+import { Country } from '../../interfaces/country';
 
 @Component({
   selector: 'app-country-page',
@@ -9,6 +10,8 @@ import { switchMap } from 'rxjs';
   styles: ``
 })
 export class CountryPageComponent implements OnInit {
+
+  public country?: Country; //Nulo en el momento de inicializarse
 
   constructor(
     private activatedRoute: ActivatedRoute,
@@ -27,8 +30,9 @@ export class CountryPageComponent implements OnInit {
           return this.router.navigateByUrl('');
         }
 
-        console.log('TENEMOS UN PAIS');
-        return;
+        return this.country = country;
+
+
       });
   }
 
