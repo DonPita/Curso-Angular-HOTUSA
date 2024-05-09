d399a1e00148bf6d93b3d074f01aa5e069d6db97
Adrian Pita <pitabermudez@gmail.com>
2024-05-09 09:29:08 +0200

    Inicio del Servicio Countries, la interfaz Country, y metodo para llamar a la API. Importación de HttpClient y HttpClientModule, y Observables

---
+++ 04-countryApp/src/app/countries/services/countries.service.ts
@@ -0,0 +1,22 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Country } from '../interfaces/country';
+
+
+@Injectable({ providedIn: 'root' })
+export class ServiceNameService {
+
+  private apiUrl: string = 'https://restcountries.com/v3.1';
+
+  constructor(private http: HttpClient) { }
+
+  /*El retorno es un Observable, de un arreglo de paises, por tanto hay que
+  decirselo al get tambien, si no da error*/
+  searchCapital(term: string): Observable<Country[]> {
+    //Mas fácil de leer de esta manera el codigo de abajo
+    const url = `${this.apiUrl}/capital/${term}`;
+    return this.http.get<Country[]>(url);
+  }
+
+}
